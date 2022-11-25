ea2a986ce3baec48b3b4a5f89b3af229b8427052
Dustin Liaw <dliaw@middlebury.edu>
2022-11-25 15:24:38 -0800

    Convert static to normal methods

--- epub_readlong_generator.py
+++ epub_readlong_generator.py
@@ -12,76 +12,82 @@ import shutil
 from tempfile import TemporaryDirectory
 import xml.etree.ElementTree as ET
 
+
 class EpubReadalongGenerator:
-    
-    @staticmethod
-    def generate_readalong(src_epub_filepath: str, audio_filepath: str, audio_timing_filepath: str):
-        logging.info(src_epub_filepath)
-            # with zipfile.ZipFile(epub_filepath, 'r') as epub, "TemporaryDirectory()" as working_dir:
-                working_dir = "test/epub"
-        shutil.unpack_archive(src_epub_filepath, working_dir, "zip")
-                logging.info("Extracted epub")
 
-                EpubReadalongGenerator.add_audio_file(working_dir, audio_filepath)
-                xhtml_stems = EpubReadalongGenerator.get_xhtml_stems(working_dir)
-                EpubReadalongGenerator.add_smil_files(working_dir, xhtml_stems)
-                EpubReadalongGenerator.edit_content_opf(working_dir, audio_filepath, xhtml_stems)
-            EpubReadalongGenerator.extract_text(working_dir, xhtml_stems, audio_timing_filepath, audio_filepath)
-        EpubReadalongGenerator.zip_epub(working_dir, src_epub_filepath)
+    def __init__(self, src_epub_filepath, src_audio_filepath, audio_timing_filepath):
+        self.src_epub_filepath = src_epub_filepath
+        self.src_audio_filepath = src_audio_filepath
+        self.audio_timing_filepath = audio_timing_filepath
+        self.epub_workdir = ""
+        self.xhtml_stems = []
 
-    @staticmethod
-    def add_audio_file(working_dir, src_audio_filepath: str):
-        epub_audio_dir = os.path.join(working_dir, "OEBPS", "audio")
-        logging.info(epub_audio_dir)
-        if not os.path.exists(epub_audio_dir):
-            os.mkdir(epub_audio_dir)
-            logging.debug("created audio dir")
-        _, audio_filename = os.path.split(src_audio_filepath)
-        destination_audio_filepath = os.path.join(epub_audio_dir, audio_filename)
-        logging.debug(src_audio_filepath)
-        logging.debug(destination_audio_filepath)
-        shutil.copy(src_audio_filepath, destination_audio_filepath)
+    def generate(self):
+        logging.info(self.src_epub_filepath)
+        # with "TemporaryDirectory()" as tempdir:
+        tempdir = "test"
+        # Create temp working directory
+        self.epub_workdir = os.path.join(tempdir, "epub")
+        # os.rmdir(self.epub_workdir)
+        os.mkdir(self.epub_workdir)
+        shutil.unpack_archive(self.src_epub_filepath,
+                                self.epub_workdir, "zip")
+        logging.debug(f"Extracted epub to {self.epub_workdir}")
 
-    @staticmethod
-    # Assumes that all xhtml files are in OEBPS/text
-    def get_xhtml_stems(working_dir)-> list[str]:
-        xhtml_stems = []
-        epub_text_dir = os.path.join(working_dir, "OEBPS", "text")
-        for root, _, files in os.walk(epub_text_dir):
+        self.add_audio_file()
+        self.get_xhtml_stems()
+        self.add_smil_files()
+        self.edit_content_opf()
+        self.extract_text()
+        self.zip_epub()
+
+    def add_audio_file(self):
+        audio_workdir = os.path.join(self.epub_workdir, "OEBPS", "audio")
+        logging.info(audio_workdir)
+        if not os.path.exists(audio_workdir):
+            os.mkdir(audio_workdir)
+            logging.debug(f"Created audio directory: {audio_workdir}")
+        dest_audio_filepath = os.path.join(
+            audio_workdir, self.get_audio_filename())
+        shutil.copy(self.src_audio_filepath, dest_audio_filepath)
+        logging.debug(
+            f"Copied {self.src_audio_filepath} to {dest_audio_filepath}")
+
+    def get_xhtml_stems(self) -> list[str]:
+        text_workdir = os.path.join(self.epub_workdir, "OEBPS", "text")
+        for root, _, files in os.walk(text_workdir):
             for f in files:
                 filetype, _ = mimetypes.guess_type(f)
                 if filetype == "application/xhtml+xml":
-                    xhtml_stems.append(Path(f).stem)
-        return sorted(xhtml_stems)
+                    self.xhtml_stems.append(Path(f).stem)
+        self.xhtml_stems = sorted(self.xhtml_stems)
 
-    @staticmethod
-    def add_smil_files(working_dir, xhtml_stems: list[str]):
-        epub_smil_dir = os.path.join(working_dir, "OEBPS", "smil")
-        if not os.path.exists(epub_smil_dir):
-            os.mkdir(epub_smil_dir)
-            logging.debug("created smil dir")
-        for filestem in xhtml_stems:
-            page_smil_filepath = EpubReadalongGenerator.get_smil_filepath(working_dir, filestem)
+    def add_smil_files(self):
+        smil_workdir = os.path.join(self.epub_workdir, "OEBPS", "smil")
+        if not os.path.exists(smil_workdir):
+            os.mkdir(smil_workdir)
+            logging.debug(f"Created smil directory: {smil_workdir}")
+        for filestem in self.xhtml_stems:
+            page_smil_filepath = self.get_smil_filepath(filestem)
             shutil.copy("resources/template.smil", page_smil_filepath)
-            logging.debug(page_smil_filepath)
+            logging.debug(f"Created {page_smil_filepath}")
 
-    @staticmethod
-    def edit_content_opf(working_dir, src_audio_filepath: str, xhtml_stems: list[str]):
-        content_opf_filepath = os.path.join(working_dir, "OEBPS", "content.opf")
+    def edit_content_opf(self):
+        logging.debug("Editing content.opf")
+        content_opf_filepath = os.path.join(
+            self.epub_workdir, "OEBPS", "content.opf")
         ET.register_namespace("", "http://www.idpf.org/2007/opf")
         content_opf_tree = ET.parse(content_opf_filepath)
         root = content_opf_tree.getroot()
-        logging.debug(root.text)
+        ns = {"opf": "http://www.idpf.org/2007/opf"}    # .opf namespace
 
-        # Namespace
-        ns = {"opf": "http://www.idpf.org/2007/opf"}
-        
         # Metadata - audio elements
         metadata_el = root.find("opf:metadata", ns)
         logging.debug(metadata_el)
         media_duration_el = ET.Element("meta")
         media_duration_el.set("property", "media:duration")
-        media_length = round(mutagen.File(src_audio_filepath).info.length) # Duration in seconds
+        # Duration in seconds
+        media_length = round(mutagen.File(self.src_audio_filepath).info.length)
         duration_hours = media_length // 3600
         duration_minutes = (media_length % 3600) // 60
         duration_seconds = media_length % 60
@@ -98,46 +104,48 @@ class EpubReadalongGenerator:
         logging.debug(metadata_el)
         audio_item_el = ET.Element("item")
         audio_item_el.set("id", "audio1")
-        _, audio_filename = os.path.split(src_audio_filepath)
+        audio_filename = self.get_audio_filename()
         audio_item_el.set("href", "audio/" + audio_filename)
         filetype, _ = mimetypes.guess_type(audio_filename)
         audio_item_el.set("media-type", filetype)
         manifest_el.append(audio_item_el)
 
         # Manifest - smil/xhtml
-        for filestem in reversed(xhtml_stems):  # Reversed so that they are added in correct order
+        # Loop through reversed list so that they are added in ascending order
+        for filestem in reversed(self.xhtml_stems):
             smil_el = ET.Element("item")
             smil_el.set("media-type", "application/smil+xml")
             smil_el.set("id", "smil_" + filestem)
             smil_el.set("href", "smil/" + filestem + ".smil")
             manifest_el.insert(0, smil_el)
             xhtml_el = manifest_el.find("opf:item[@id='" + filestem + "']", ns)
-            logging.debug(xhtml_el)
             xhtml_el.set("media-overlay", "smil_" + filestem)
 
         content_opf_tree.write(content_opf_filepath, "utf-8", True)
 
-    @staticmethod
-    def extract_text(working_dir, xhtml_stems: list[str], audio_timing_filepath: str, src_audio_filepath: str):
-        logging.debug("extract_text")
+    def extract_text(self):
+        logging.debug("Extract text")
         text_id = 1
         audio_timings = None
-        with open(audio_timing_filepath) as timing_fp:
+        with open(self.audio_timing_filepath) as timing_fp:
             audio_timings = timing_fp.readlines()
 
         # Loop through xhtml files and:
         # 1) Wrap individual words in spans
         # 2) Add corresponding audio timing par to smil
-        for filestem in xhtml_stems:
-            xhtml_filepath = EpubReadalongGenerator.get_xhtml_filepath(working_dir, filestem)
-            smil_filepath = EpubReadalongGenerator.get_smil_filepath(working_dir, filestem)
-            xhtml_xmltree = etree.parse(xhtml_filepath, XHTMLParser(resolve_entities=False))
+        for filestem in self.xhtml_stems[:12]:
+            xhtml_filepath = self.get_xhtml_filepath(
+                filestem)
+            smil_filepath = self.get_smil_filepath(filestem)
+            xhtml_xmltree = etree.parse(
+                xhtml_filepath, XHTMLParser(resolve_entities=False))
             smil_xmltree = etree.parse(smil_filepath)
-            text_nodes = xhtml_xmltree.xpath("//*[local-name()='body']//text()")
+            text_nodes = xhtml_xmltree.xpath(
+                "//*[local-name()='body']//text()")
             smil_body = smil_xmltree.xpath("//*[local-name()='body']")[0]
-            _, audio_filename = os.path.split(src_audio_filepath)
+            audio_filename = self.get_audio_filename()
             logging.debug(text_nodes)
-            
+
             for text in text_nodes:
                 logging.debug("text: " + text)
                 if not text.isspace():
@@ -146,7 +154,7 @@ class EpubReadalongGenerator:
                     if text.is_tail:
                         prev_sibling = text.getparent()
                         prev_sibling.tail = None    # Remove existing text so it can be wrapped
-                    
+
                     words = edited_text.split()
                     logging.debug(words)
                     # Loop through words and create sibling spans beneath the parent
@@ -163,15 +171,16 @@ class EpubReadalongGenerator:
                         par_el = etree.SubElement(smil_body, "par")
                         par_el.set("id", "par" + str(text_id))
                         par_text_el = etree.SubElement(par_el, "text")
-                        par_text_el.set("src", f"../text/{filestem}.xhtml#w{text_id}")
+                        par_text_el.set(
+                            "src", f"../text/{filestem}.xhtml#w{text_id}")
                         par_audio_el = etree.SubElement(par_el, "audio")
                         par_audio_el.set("src", f"../audio/{audio_filename}")
                         audio_timing = audio_timings[text_id - 1].split()
                         par_audio_el.set("clipBegin", f"{audio_timing[0]}s")
                         par_audio_el.set("clipEnd", f"{audio_timing[1]}s")
-                        
+
                         text_id += 1
-                        
+
                         if prev_sibling is None:
                             text_parent_el = text.getparent()
                             text_parent_el.text = None  # Remove existing text so it can be wrapped
@@ -181,7 +190,7 @@ class EpubReadalongGenerator:
                                 text_parent_el.text = edited_text[:word_idx]
                         else:
                             prev_sibling.addnext(span_el)
-                            if word_idx != 0: 
+                            if word_idx != 0:
                                 prev_sibling.tail = edited_text[:word_idx]
                         # Remove words/whitespace which have been processed
                         edited_text = edited_text[word_idx + len(word):]
@@ -189,26 +198,31 @@ class EpubReadalongGenerator:
                     if len(edited_text):
                         # Leftover whitespace
                         prev_sibling.tail = edited_text
-            xhtml_xmltree.write(xhtml_filepath, encoding="utf-8", standalone=True)
+            xhtml_xmltree.write(
+                xhtml_filepath, encoding="utf-8", standalone=True)
             smil_xmltree.write(smil_filepath, encoding="utf-8")
-    
-    @staticmethod
-    def zip_epub(working_dir, src_epub_filepath: str):
-        src_epub_folder, src_epub_filename = os.path.split(src_epub_filepath)
-        readalong_epub_filename = Path(src_epub_filename).stem + "_readalong.epub"
-        temp_epub_filepath = os.path.join(working_dir, "readalong.zip")
-        shutil.make_archive(os.path.join(working_dir, "readalong"), "zip", working_dir)
-        shutil.copyfile(temp_epub_filepath, os.path.join(src_epub_folder, readalong_epub_filename))
 
-    
-    @staticmethod
-    def get_xhtml_filepath(working_dir, filestem: str)-> str:
-        return os.path.join(working_dir, "OEBPS", "text", filestem + ".xhtml")
-    
-    @staticmethod
-    def get_smil_filepath(working_dir, filestem: str)-> str:
-        return os.path.join(working_dir, "OEBPS", "smil", filestem + ".smil")    
-        
+    def zip_epub(self):
+        src_epub_folder, src_epub_filename = os.path.split(
+            self.src_epub_filepath)
+        readalong_epub_filename = Path(
+            src_epub_filename).stem + "_readalong.epub"
+        temp_epub_filepath = os.path.join(self.epub_workdir, "readalong.zip")
+        shutil.make_archive(os.path.join(self.epub_workdir,
+                            "readalong"), "zip", self.epub_workdir)
+        shutil.copyfile(temp_epub_filepath, os.path.join(
+            src_epub_folder, readalong_epub_filename))
+
+    def get_xhtml_filepath(self, filestem: str) -> str:
+        return os.path.join(self.epub_workdir, "OEBPS", "text", filestem + ".xhtml")
+
+    def get_smil_filepath(self, filestem: str) -> str:
+        return os.path.join(self.epub_workdir, "OEBPS", "smil", filestem + ".smil")
+
+    def get_audio_filename(self) -> str:
+        _, audio_filename = os.path.split(self.src_audio_filepath)
+        return audio_filename
+
 
 if __name__ == "__main__":
     # TODO: Implement argparse
@@ -220,4 +234,5 @@ if __name__ == "__main__":
     # print(args)
 
     logging.basicConfig(level=logging.DEBUG)
-    EpubReadalongGenerator.generate_readalong("./test/The Fire Engine Book_original copy.epub", "test/audio.m4a", "test/audio_full.txt")
+    EpubReadalongGenerator("./test/The Fire Engine Book_original copy.epub",
+                           "test/audio.m4a", "test/audio_full.txt").generate()
